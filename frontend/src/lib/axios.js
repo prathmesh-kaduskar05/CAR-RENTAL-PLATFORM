197eb2d70f881ee1656410997296c5dc76103d15
prathmesh-kaduskar05 <chmcs2026.prathmesh.kaduskar@gmail.com>
2026-03-12 19:15:31 +0530

    Change the baseURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     
-    baseURL: 'https://car-rental-platform-9rjo.onrender.com/'
+    baseURL: 'http://localhost:3000/cars'
 })
 
 export default api;
